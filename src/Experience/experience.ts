2b977a5434f4cd86d86d94d25994854ef5da423d
John <johnfr14.dev@proton.me>
2022-10-12 14:31:24 +0700

    put experience as singeton

--- src/Experience/experience.ts
+++ src/Experience/experience.ts
@@ -5,18 +5,24 @@ import Time from "./Utils/Time"
 import Camera from "./Camera"
 import Renderer from "./Renderer"
 import World from "./World/World"
-import Ressources from "./Utils/Ressources"
+import Resources from "./Utils/Resources"
 import Debug from "./Utils/Debug"
 import Mouse from "./Utils/Mouse"
 import Raycaster from "./Utils/Raycaster"
 
+declare global {
+  interface Window { experience: any }
+}
+
 export default class Experience {
+  private static _instance: Experience | null;
+  
   debug: Debug
   canvas: HTMLCanvasElement
   sizes: Sizes
   time: Time
   scene: THREE.Scene
-  ressources: Ressources
+  resources: Resources
   mouse: Mouse
   camera: Camera
   renderer: Renderer
@@ -24,32 +30,44 @@ export default class Experience {
   raycaster: Raycaster
   
   constructor(canvas: HTMLCanvasElement) {
+     
+    // Global access
+    window.experience = this
+
     this.debug = new Debug()
     this.canvas = canvas
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
-    this.ressources = new Ressources(sources)
+    this.resources = new Resources(sources)
     this.mouse = new Mouse(this)
     this.camera = new Camera(this)
     this.renderer = new Renderer(this)
     this.world = new World(this)
     this.raycaster = new Raycaster(this)
-
+    
     this.sizes.on('resize', () => this.resize())
     this.time.on("tick", () => this.update())
+
+    Experience._instance = this
   }
 
   resize(): void {
-    this.camera.resize()
-    this.renderer.resize()
+    this.camera!.resize()
+    this.renderer!.resize()
   }
 
   update(): void {
-    this.raycaster.update()
-    this.camera.update()
-    this.world.update()
-    this.renderer.update()
+    this.raycaster!.update()
+    this.camera!.update()
+    this.world!.update()
+    this.renderer!.update()
+  }
+
+  public static Instance(canvas?: HTMLCanvasElement)
+  {
+      // Do you need arguments? Make it a regular static method instead.
+      return this._instance || (this._instance = new this(canvas!));
   }
 }
 
